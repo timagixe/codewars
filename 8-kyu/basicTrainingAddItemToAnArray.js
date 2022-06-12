feee48fedaa0fcea9e4a80899f96d10ad26aeb8f
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 22:14:09 +0300

    Add Basic Training: Add item to an Array

---
+++ 8-kyu/basicTrainingAddItemToAnArray.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+Basic Training: Add item to an Array
+
+- DESCRIPTION:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+- LABELS:
+#ARRAYS #FUNDAMENTALS
+
+*/
+
+// add the value "codewars" to the already defined websites array
+websites.push("codewars");
