2e9be656e5606c42e9f1742c9fa095df4ef3f269
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 22:18:42 +0300

    Add Return to Sanity

---
+++ 8-kyu/returnToSanity.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Return to Sanity
+
+- DESCRIPTION:
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+- LABELS:
+#BUGS
+
+*/
+
+function mystery() {
+  return { sanity: "Hello" };
+}
