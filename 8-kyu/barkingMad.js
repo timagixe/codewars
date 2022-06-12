6a741a909602140f0327bbf0b556fa057cf802d1
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 23:44:54 +0300

    Add Barking mad

---
+++ 8-kyu/barkingMad.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+Barking mad
+
+- DESCRIPTION:
+Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function Dog(breed) {
+  this.breed = breed;
+}
+
+const snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function () {
+  return "Woof";
+};
+
+const scoobydoo = new Dog("Great Dane");
