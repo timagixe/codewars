bf58cbac74a3a8f634c31a24e47c78d97e188d67
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 22:32:02 +0300

    Add Color Ghost

---
+++ 8-kyu/colorGhost.js
@@ -0,0 +1,30 @@
+/*
+
+- TITLE:
+Color Ghost
+
+- DESCRIPTION:
+Color Ghost
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+- LABELS:
+#OBJECT-ORIENTED PROGRAMMING #FUNDAMENTALS
+
+*/
+
+const COLORS = ["white", "yellow", "purple", "red"];
+
+const randomIndex = (array) => Math.round(Math.random() * (array.length - 1));
+
+const randomColor = () => COLORS[randomIndex(COLORS)];
+
+function Ghost() {
+  this.color = randomColor();
+}
