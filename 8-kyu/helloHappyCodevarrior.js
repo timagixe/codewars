4d25e2f376fc8c35b70adaf47c7bd05c894df481
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 22:46:32 +0300

    Add Hello Happy Codevarrior!

---
+++ 8-kyu/helloHappyCodevarrior.js
@@ -0,0 +1,33 @@
+/*
+
+- TITLE:
+Hello Happy Codevarrior!
+
+- DESCRIPTION:
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+- LABELS:
+#BUGS
+
+*/
+
+function Warrior(n) {
+  let name = n;
+  this.name = function (n) {
+    if (n) name = n;
+    return name;
+  };
+}
+
+Warrior.prototype.toString = function () {
+  return "Hi! my name's " + this.name();
+};
