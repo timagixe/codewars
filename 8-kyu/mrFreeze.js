27398dce5b4f6715c43ebbf721214c2e92c9020b
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 21:59:17 +0300

    Add Mr. Freeze

---
+++ 8-kyu/mrFreeze.js
@@ -0,0 +1,15 @@
+/*
+
+- TITLE:
+Mr. Freeze
+
+- DESCRIPTION:
+There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+- LABELS:
+FUNDAMENTALS
+
+*/
+
+// mark the MrFreeze object instance as frozen
+Object.freeze(MrFreeze);
