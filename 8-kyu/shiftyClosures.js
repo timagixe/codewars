7e1c8678c13b4231dd8fe874ae6a2960fa284573
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-12 21:39:53 +0300

    Add Shifty Closures

---
+++ 8-kyu/shiftyClosures.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+Shifty Closures
+
+- DESCRIPTION:
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greet_abe() actually greet Abe?
+
+- LABELS:
+#CLOSURES #BUGS
+
+*/
+
+function great(name) {
+  return function () {
+    return "Hello, " + name + "!";
+  };
+}
+
+const greet_abe = great("Abe");
+const greet_ben = great("Ben");
