f90b4de9fbbf7806e0595b4ab1d233a059c276c3
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-13 09:20:38 +0300

    Add Switch/Case - Bug Fixing #6

---
+++ 8-kyu/switchCaseBugFixing6.js
@@ -0,0 +1,40 @@
+/*
+
+- TITLE:
+Switch/Case - Bug Fixing #6
+
+- DESCRIPTION:
+Switch/Case - Bug Fixing #6
+Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
+- LABELS:
+#BUGS
+
+*/
+
+function evalObject(value) {
+  let result;
+  switch (value.operation) {
+    case "+":
+      result = value.a + value.b;
+      break;
+    case "-":
+      result = value.a - value.b;
+      break;
+    case "/":
+      result = value.a / value.b;
+      break;
+    case "*":
+      result = value.a * value.b;
+      break;
+    case "%":
+      result = value.a % value.b;
+      break;
+    case "^":
+      result = Math.pow(value.a, value.b);
+      break;
+    default:
+      result = 0;
+  }
+  return result;
+}
