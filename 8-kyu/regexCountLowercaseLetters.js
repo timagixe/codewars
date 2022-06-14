6f10f2e74f68b1e91f851e112a0c8d2b6c6bd677
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-14 08:59:56 +0300

    Add Regex count lowercase letters

---
+++ 8-kyu/regexCountLowercaseLetters.js
@@ -0,0 +1,29 @@
+/*
+
+- TITLE:
+Regex count lowercase letters
+
+- DESCRIPTION:
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+lowercaseCount("abc"); ===> 3
+
+lowercaseCount("abcABC123"); ===> 3
+
+lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+lowercaseCount(""); ===> 0;
+
+lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+- LABELS:
+#FUNDAMENTALS #REGULAR_EXPRESSIONS #ALGORITHMS
+
+*/
+
+function lowercaseCount(str) {
+  return (str.match(/[a-z]/g) || []).length;
+}
