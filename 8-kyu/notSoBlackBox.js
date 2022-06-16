0997493dfab886402fc2fc8269b72dd3130112eb
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-16 17:53:49 +0300

    Add Not so black box

---
+++ 8-kyu/notSoBlackBox.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Not so black box
+
+- DESCRIPTION:
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+You may need to think laterally!
+
+- LABELS:
+#PUZZLES
+
+*/
+
+console.log(blackBox.toString());
+
+JSopenSesame();
