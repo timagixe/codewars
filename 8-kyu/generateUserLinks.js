5cd8dc8dfe499c2c36d1335cee0d29e8ae6ec0d9
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-16 17:10:54 +0300

    Add Generate user links

---
+++ 8-kyu/generateUserLinks.js
@@ -0,0 +1,22 @@
+/*
+
+- TITLE:
+Generate user links
+
+- DESCRIPTION:
+Generate user links
+Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+Example
+generate_link('matt c')
+http://www.codewars.com/users/matt%20c
+reference
+use this as a reference encoding
+
+- LABELS:
+#PUZZLES
+
+*/
+function generateLink(user) {
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
+}
