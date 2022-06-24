ff45fc0057ef94a0edb07af4d1852ff8afb101ca
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-24 11:35:13 +0300

    Add Is your period late?

---
+++ 8-kyu/isYourPeriodLate.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+Is your period late?
+
+- DESCRIPTION:
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+
+- LABELS:
+#FUNDAMENTALS #DATE_TIME
+
+*/
+
+const DAY_TO_MILLISECONDS = 86400000;
+
+function periodIsLate(last, today, cycleLength) {
+  return (today.getTime() - last.getTime()) / DAY_TO_MILLISECONDS > cycleLength;
+}
