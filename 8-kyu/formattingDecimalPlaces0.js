b64f42a70bfcdb88264f30d5e74a4dce361098ef
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-06-28 19:13:35 +0300

    Add Formatting decimal places #0

---
+++ 8-kyu/formattingDecimalPlaces0.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+Formatting decimal places #0
+
+- DESCRIPTION:
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+
+- LABELS:
+#FUNDAMENTALS #FORMATTING
+
+*/
+
+function twoDecimalPlaces(n) {
+  return Number(n.toFixed(2));
+}
