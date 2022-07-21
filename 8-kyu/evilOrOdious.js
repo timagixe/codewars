13445fd0c29910b05b9ca64cbb7f6bd70ce89abe
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-07-21 10:50:18 +0300

    Add Evil or Odious

---
+++ 8-kyu/evilOrOdious.js
@@ -0,0 +1,35 @@
+/*
+
+- TITLE:
+Evil or Odious
+
+- DESCRIPTION:
+The number n is Evil if it has an even number of 1's in its binary representation.
+The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
+
+The number n is Odious if it has an odd number of 1's in its binary representation.
+The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+
+You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+
+good luck :)
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+const ODIOUS = "Odious";
+const EVIL = "Evil";
+
+function isOdious(binaryNumber) {
+  return binaryNumber
+    .split("")
+    .reduce((acc, num) => (num === "1" ? !acc : acc), false);
+}
+
+function evil(n) {
+  const binaryN = n.toString(2);
+
+  return `It's ${isOdious(binaryN) ? ODIOUS : EVIL}!`;
+}
