91e118bb018684a7c9f5c88bd7acf52591625970
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-17 17:38:07 +0300

    Add !a == a ?!

---
+++ 8-kyu/aA.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+!a == a ?!
+
+- DESCRIPTION:
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+
+- LABELS:
+#FUNDAMENTALS #LANGUAGE_FEATURES
+
+*/
+
+const a = [];
