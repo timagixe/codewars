ccdec55845009e44dc0b7083182d0e4626d41f2e
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-18 15:40:22 +0300

    Add Type of sum

---
+++ 8-kyu/typeOfSum.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Type of sum
+
+- DESCRIPTION:
+Return the type of the sum of the two arguments
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function typeOfSum(a, b) {
+  return typeof (a + b);
+}
