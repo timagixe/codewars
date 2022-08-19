984fd819e77e39f3216d1c12f83e99677581ecca
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-19 11:46:51 +0300

    Add Geometry Basics: Circle Circumference in 2D

---
+++ 8-kyu/geometryBasicsCircleCircumferenceIn2D.js
@@ -0,0 +1,22 @@
+/*
+
+- TITLE:
+Geometry Basics: Circle Circumference in 2D
+
+- DESCRIPTION:
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+
+Write a function calculating circumference of a Circle.
+
+Tests round answers to 6 decimal places.
+
+- LABELS:
+#GEOMETRY #FUNDAMENTALS
+
+*/
+
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
