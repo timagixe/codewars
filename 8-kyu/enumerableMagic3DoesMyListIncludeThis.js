53239509bf35a3dd0ae745cccca614dd0f899bf2
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-22 09:46:30 +0300

    Add Enumerable Magic #3 - Does My List Include This?

---
+++ 8-kyu/enumerableMagic3DoesMyListIncludeThis.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Enumerable Magic #3 - Does My List Include This?
+
+- DESCRIPTION:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function include(arr, item) {
+  return arr.includes(item);
+}
