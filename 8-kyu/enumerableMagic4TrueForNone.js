77a6e58baabc6353ffb571a133a342cec70bcd69
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-24 11:51:54 +0300

    Add Enumerable Magic #4 - True for None?

---
+++ 8-kyu/enumerableMagic4TrueForNone.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Enumerable Magic #4 - True for None?
+
+- DESCRIPTION:
+Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function none(array, callback) {
+  return !array.some(callback);
+}
