b94794456b372541d1ca0e4741cde7eb20e2485a
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-08-29 10:46:39 +0300

    Add Closest elevator

---
+++ 8-kyu/closestElevator.js
@@ -0,0 +1,48 @@
+/*
+
+- TITLE:
+Closest elevator
+
+- DESCRIPTION:
+Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+
+left - The current floor of the left elevator
+right - The current floor of the right elevator
+call - The floor that called an elevator
+It should return the name of the elevator closest to the called floor ("left"/"right").
+
+In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+You can assume that the inputs will always be valid integers between 0-2.
+
+Examples:
+
+elevator(0, 1, 0); // => "left"
+elevator(0, 1, 1); // => "right"
+elevator(0, 1, 2); // => "right"
+elevator(0, 0, 0); // => "right"
+elevator(0, 2, 1); // => "right"
+
+- LABELS:
+#ALGORITHMS
+
+*/
+
+const ELEVATOR = {
+  RIGHT: "right",
+  LEFT: "left"
+};
+
+function areEquallyDistant(left, right, call) {
+  return left === right || Math.abs(call - left) === Math.abs(call - right);
+}
+
+function elevator(left, right, call) {
+  if (areEquallyDistant(left, right, call)) return ELEVATOR.RIGHT;
+
+  const isRightCloser = Math.abs(call - left) > Math.abs(call - right);
+
+  if (isRightCloser) return ELEVATOR.RIGHT;
+
+  return ELEVATOR.LEFT;
+}
