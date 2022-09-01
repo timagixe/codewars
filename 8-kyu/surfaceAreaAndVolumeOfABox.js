bb3f61599725490d701a706716788d5cfbe6a619
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-01 13:38:52 +0300

    Add Surface Area and Volume of a Box

---
+++ 8-kyu/surfaceAreaAndVolumeOfABox.js
@@ -0,0 +1,27 @@
+/*
+
+- TITLE:
+Surface Area and Volume of a Box
+
+- DESCRIPTION:
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+- LABELS:
+#GEOMETRY #FUNDAMENTALS
+
+*/
+
+function calculateBoxArea(width, height, depth) {
+  return 2 * width * height + 2 * height * depth + 2 * depth * width;
+}
+
+function calculateBoxVolume(width, height, depth) {
+  return width * height * depth;
+}
+
+function getSize(width, height, depth) {
+  return [
+    calculateBoxArea(width, height, depth),
+    calculateBoxVolume(width, height, depth)
+  ];
+}
