646d3af9aa9d8d9e37431e07d84a383e17ca7625
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-04 12:48:11 +0300

    Add get ascii value of character

---
+++ 8-kyu/getAsciiValueOfCharacter.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+get ascii value of character
+
+- DESCRIPTION:
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function getASCII(character) {
+  return character.charCodeAt(0);
+}
