846f4a4d95befeefcb7681b1bf73a0b77985c99d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-04 12:46:37 +0300

    Add Triple Trouble

---
+++ 8-kyu/tripleTrouble.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Triple Trouble
+
+- DESCRIPTION:
+Triple Trouble
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+
+- LABELS:
+#PUZZLES
+
+*/
+
+function tripleTrouble(one, two, three) {
+  const length = one.length;
+  return Array.from({ length }, (value, key) =>
+    [one[key], two[key], three[key]].join("")
+  ).join("");
+}
