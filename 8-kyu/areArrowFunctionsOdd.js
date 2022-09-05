606feaacbdf3b572a6ddf65f98360597378c137e
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-05 10:34:57 +0300

    Add Are arrow functions odd?

---
+++ 8-kyu/areArrowFunctionsOdd.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+Are arrow functions odd?
+
+- DESCRIPTION:
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function isOdd(value) {
+  return Boolean(value % 2);
+}
+
+function odds(values) {
+  return values.filter(isOdd);
+}
