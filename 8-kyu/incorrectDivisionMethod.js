9e19eb6cb8409da58a17ae2f50a0e5583b4f286d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-06 08:09:58 +0300

    Add Incorrect division method

---
+++ 8-kyu/incorrectDivisionMethod.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Incorrect division method
+
+- DESCRIPTION:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+- LABELS:
+#DEBUGGING
+
+*/
+
+function solve(x, y) {
+  return x / y;
+}
