0240623ed1c8c11dc5b47dfe6298ce74eb6a9189
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-12 11:49:15 +0300

    Add Ensure question

---
+++ 8-kyu/ensureQuestion.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Ensure question
+
+- DESCRIPTION:
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+const QUESTION_MARK = "?";
+
+function ensureQuestion(string) {
+  if (string.endsWith(QUESTION_MARK)) return string;
+  return `${string}?`;
+}
