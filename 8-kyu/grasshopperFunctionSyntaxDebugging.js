daad5da0237228bfc93ccabf60a018cee3db3b69
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2022-09-12 11:36:22 +0300

    Update Grasshopper - Function syntax debugging

---
+++ 8-kyu/grasshopperFunctionSyntaxDebugging.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Grasshopper - Function syntax debugging
+
+- DESCRIPTION:
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function main(verb, noun) {
+  return verb + noun;
+}
