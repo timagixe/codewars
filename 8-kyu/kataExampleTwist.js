07646aa59e7f05332ac1222dd134045d09d5c646
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-06 10:36:45 +0200

    Add Kata Example Twist

---
+++ 8-kyu/kataExampleTwist.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Kata Example Twist
+
+- DESCRIPTION:
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+const websites = new Array(1000).fill("codewars");
