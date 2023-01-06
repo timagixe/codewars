efbbfbcfb83cd83c797de9dd46d7300383d2ddb7
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-06 22:27:48 +0200

    Add Grasshopper - Variable Assignment Debug

---
+++ 8-kyu/grasshopperVariableAssignmentDebug.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Grasshopper - Variable Assignment Debug
+
+- DESCRIPTION:
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+const a = "dev";
+const b = "Lab";
+
+const name = a + b;
