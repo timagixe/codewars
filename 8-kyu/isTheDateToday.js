4ed9e2d3e4a1a9ef418b0c0507cd2b2958bcca90
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-09 09:02:43 +0200

    Add Is the date today

---
+++ 8-kyu/isTheDateToday.js
@@ -0,0 +1,26 @@
+/*
+
+- TITLE:
+Is the date today
+
+- DESCRIPTION:
+Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+Make sure that your function does not return a false positive by only checking the day.
+
+- LABELS:
+#DATE_TIME #PUZZLES
+
+*/
+
+function isToday(date) {
+  return date.toDateString() === new Date().toDateString();
+}
+
+function extractDate(date) {
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth(),
+    day: date.getDay()
+  };
+}
