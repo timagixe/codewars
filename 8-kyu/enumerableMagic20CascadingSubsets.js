5b486b5279553e07e32e8a5c0e70adcaf98eb506
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-12 08:37:06 +0200

    Add Enumerable Magic #20 - Cascading Subsets

---
+++ 8-kyu/enumerableMagic20CascadingSubsets.js
@@ -0,0 +1,30 @@
+/*
+
+- TITLE:
+Enumerable Magic #20 - Cascading Subsets
+
+- DESCRIPTION:
+Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+each_cons([1,2,3,4], 2)
+  #=> [[1,2], [2,3], [3,4]]
+
+each_cons([1,2,3,4], 3)
+  #=> [[1,2,3],[2,3,4]]
+  
+As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+- LABELS:
+#FUNDAMENTALS #LISTS #DATA_STRUCTURES #ARRAYS #ALGORITHMS
+
+*/
+
+function eachCons(array, length) {
+  const result = [];
+  for (let index = 0; index <= array.length; index += 1) {
+    const chunk = array.slice(index, length + index);
+    if (chunk.length !== length) return result;
+    result.push(chunk);
+  }
+  return result;
+}
