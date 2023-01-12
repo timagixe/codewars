5a0d40586c38138a03703a8e18393c8599118e55
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-12 08:06:18 +0200

    Add 101 Dalmatians - squash the bugs, not the dogs!

---
+++ 8-kyu/101DalmatiansSquashTheBugsNotTheDogs.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+101 Dalmatians - squash the bugs, not the dogs!
+
+- DESCRIPTION:
+Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+The number of dogs will always be a number and there will always be at least 1 dog.
+
+- LABELS:
+#DEBUGGING #FUNDAMENTALS
+
+*/
+
+function howManyDalmatians(count) {
+  if (count === 101) return "101 DALMATIANS!!!";
+  if (count <= 10) return "Hardly any";
+  if (count <= 50) return "More than a handful!";
+  return "Woah that's a lot of dogs!";
+}
