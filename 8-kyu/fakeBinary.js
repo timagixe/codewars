333cc26f71c6837239113fd9603efd234bb5d5b5
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-16 09:27:35 +0200

    Add Fake Binary

---
+++ 8-kyu/fakeBinary.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Fake Binary
+
+- DESCRIPTION:
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+- LABELS:
+#FUNDAMENTALS #STRINGS #ARRAYS
+
+*/
+
+function fakeBin(x) {
+  return x.replace(/\d/g, (match) => (Number(match) >= 5 ? 1 : 0));
+}
