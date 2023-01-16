f5f75e1e6693a67fa24b518059a05fe3cf00ffd9
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-16 13:29:12 +0200

    Add Is this my tail?

---
+++ 8-kyu/isThisMyTail.js
@@ -0,0 +1,21 @@
+/*
+
+- TITLE:
+Is this my tail?
+
+- DESCRIPTION:
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be non empty strings, and normal letters.
+
+- LABELS:
+#DEBUGGING
+
+*/
+
+function correctTail(body, tail) {
+  const bodyTail = body.substr(body.length - tail.length);
+  return bodyTail === tail;
+}
