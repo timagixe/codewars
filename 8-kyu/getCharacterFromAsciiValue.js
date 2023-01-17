997db45362bed505e8a95ff50885f3aa04c4a51d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-17 07:30:36 +0200

    Add get character from ASCII Value

---
+++ 8-kyu/getCharacterFromAsciiValue.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+get character from ASCII Value
+
+- DESCRIPTION:
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+should return:
+
+'A'
+For ASCII table, you can refer to http://www.asciitable.com/
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function getChar(code) {
+  return String.fromCharCode(code);
+}
