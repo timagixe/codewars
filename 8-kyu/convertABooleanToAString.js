c282ac8c98a225d698f7265222dfed51de64270c
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-17 07:47:05 +0200

    Add Convert a Boolean to a String

---
+++ 8-kyu/convertABooleanToAString.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Convert a Boolean to a String
+
+- DESCRIPTION:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+- LABELS:
+#STRINGS #FUNDAMENTALS
+
+*/
+
+function booleanToString(bool) {
+  return bool.toString();
+}
