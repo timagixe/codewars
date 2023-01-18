8dfab604f93262ae03ac20ddb73f7150d583cf4c
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-18 08:21:21 +0200

    Add Hex to Decimal

---
+++ 8-kyu/hexToDecimal.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Hex to Decimal
+
+- DESCRIPTION:
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
