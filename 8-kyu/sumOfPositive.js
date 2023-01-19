23a1d4227df65531ad05869e09ea853717eeabb3
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-19 06:59:23 +0200

    Add Sum of positive

---
+++ 8-kyu/sumOfPositive.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Sum of positive
+
+- DESCRIPTION:
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+- LABELS:
+#ARRAYS #FUNDAMENTALS
+
+*/
+
+function positiveSum(values) {
+  let sum = 0;
+  for (let value of values) {
+    if (value >= 0) sum += value;
+  }
+  return sum;
+}
