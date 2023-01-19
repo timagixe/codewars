fd1f8e69c6c26b821643067bfdfa0ee2078a92ae
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-19 07:38:44 +0200

    Add What is between?

---
+++ 8-kyu/whatIsBetween.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+What is between?
+
+- DESCRIPTION:
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+- LABELS:
+#FUNDAMENTALS #ALGORITHMS
+
+*/
+
+function between(a, b) {
+  const length = Math.abs(a - b) + 1;
+  return Array.from({ length }, (_, index) => a + index);
+}
