92423246e50f8a2500822da0ec84f9f1c7b717bd
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-20 19:39:52 +0200

    Add Bin to Decimal

---
+++ 8-kyu/binToDecimal.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Bin to Decimal
+
+- DESCRIPTION:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+- LABELS:
+#BINARY #FUNDAMENTALS
+
+*/
+
+function binToDec(binary) {
+  return parseInt(binary, 2);
+}
