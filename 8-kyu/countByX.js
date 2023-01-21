c3da5c6528d67a72d157d9ef78df66da98da2847
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-21 12:25:10 +0200

    Add Count by X

---
+++ 8-kyu/countByX.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Count by X
+
+- DESCRIPTION:
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+
+- LABELS:
+#ARRAYS #FUNDAMENTALS
+
+*/
+
+function countBy(multiple, length) {
+  return Array.from({ length }, (_, index) => (index + 1) * multiple);
+}
