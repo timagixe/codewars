5eeffcbed225e5a50772dffff70ef2dab2f8256b
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-21 12:22:46 +0200

    Add Convert a string to an array

---
+++ 8-kyu/convertAStringToAnArray.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+Convert a string to an array
+
+- DESCRIPTION:
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+- LABELS:
+#ARRAYS #STRINGS #FUNDAMENTALS
+
+*/
+
+const SPACE = " ";
+
+function stringToArray(string) {
+  return string.split(SPACE);
+}
