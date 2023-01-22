fcb7b960386839fce03883e9b35b081abcddd062
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-22 10:25:41 +0200

    Add Grasshopper - Grade book

---
+++ 8-kyu/grasshopperGradeBook.js
@@ -0,0 +1,38 @@
+/*
+
+- TITLE:
+Grasshopper - Grade book
+
+- DESCRIPTION:
+Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function getGrade(...grades) {
+  const averageGrade = average(grades);
+  if (averageGrade >= 90) return "A";
+  if (averageGrade >= 80) return "B";
+  if (averageGrade >= 70) return "C";
+  if (averageGrade >= 60) return "D";
+  return "F";
+}
+
+function average(grades) {
+  return grades.reduce(sum) / grades.length;
+}
+
+function sum(a, b) {
+  return a + b;
+}
