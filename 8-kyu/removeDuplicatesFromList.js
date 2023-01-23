7ec8001bdb147c65f3066c8c853d6229704f23c3
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-23 09:54:06 +0200

    Add Remove duplicates from list

---
+++ 8-kyu/removeDuplicatesFromList.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Remove duplicates from list
+
+- DESCRIPTION:
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+- LABELS:
+#FUNDAMENTALS #ARRAYS #LISTS
+
+*/
+
+function distinct(array) {
+  return Array.from(new Set(array));
+}
