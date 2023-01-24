4e630283b9d6a33082238a11c5dcfce8a67205d2
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-24 10:03:17 +0200

    Add Wilson primes

---
+++ 8-kyu/wilsonPrimes.js
@@ -0,0 +1,33 @@
+/*
+
+- TITLE:
+Wilson primes
+
+- DESCRIPTION:
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then,
+
+((P-1)! + 1) / (P * P)
+should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+function amIWilson(num) {
+  const bigNum = BigInt(num);
+  return wilsonFormula(bigNum) === 0n;
+}
+
+function factorial(num) {
+  if (num === 0n) return 1n;
+  return num * factorial(num - 1n);
+}
+
+function wilsonFormula(num) {
+  return (factorial(num - 1n) + 1n) % (num * num);
+}
