2093b00a711ab907e55e9378ad16f48e01e90dc8
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-24 09:30:59 +0200

    Add Is it even?

---
+++ 8-kyu/isItEven.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Is it even?
+
+- DESCRIPTION:
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function testEven(num) {
+  return !(num % 2);
+}
