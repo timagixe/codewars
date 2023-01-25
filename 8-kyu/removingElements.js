5b46bc3e11dce52ec912ef37417dfdb0a8dac233
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-25 09:35:00 +0200

    Add Removing Elements

---
+++ 8-kyu/removingElements.js
@@ -0,0 +1,21 @@
+/*
+
+- TITLE:
+Removing Elements
+
+- DESCRIPTION:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+- LABELS:
+#LISTS #ARRAYS #FUNDAMENTALS
+
+*/
+
+function removeEveryOther(array) {
+  return array.filter((_, index) => index % 2 === 0);
+}
