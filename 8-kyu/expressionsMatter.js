06cf92d8935edf2e80ec92c95273b13fb2fb25e2
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-26 12:38:36 +0200

    Add Expressions Matter

---
+++ 8-kyu/expressionsMatter.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Expressions Matter
+
+- DESCRIPTION:
+Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+- LABELS:
+#ALGORITHMS #FUNDAMENTALS
+
+*/
+
+function expressionMatter(a, b, c) {
+  return Math.max(a * (b + c), a * b * c, a + b + c, (a + b) * c);
+}
