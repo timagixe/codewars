9abe31cd417aca7b47b567c98568d0c489c3e224
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-26 14:11:49 +0200

    Add Area or Perimeter

---
+++ 8-kyu/areaOrPerimeter.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+Area or Perimeter
+
+- DESCRIPTION:
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+- LABELS:
+#FUNDAMENTALS #MATHEMATICS #GEOMETRY
+
+*/
+
+function areaOrPerimeter(l, w) {
+  if (isRectangle(l, w)) return 2 * (l + w);
+  return l ** 2;
+}
+
+function isRectangle(l, w) {
+  return l !== w;
+}
