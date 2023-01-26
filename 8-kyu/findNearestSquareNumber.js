7bfc5fba31d64d6f60bad587c6a7bb772b01af23
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-26 12:41:35 +0200

    Add Find Nearest square number

---
+++ 8-kyu/findNearestSquareNumber.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Find Nearest square number
+
+- DESCRIPTION:
+Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+Goodluck :)
+
+- LABELS:
+#FUNDAMENTALS #MATHEMATICS
+
+*/
+
+function nearestSq(n) {
+  return Math.round(Math.sqrt(n)) ** 2;
+}
