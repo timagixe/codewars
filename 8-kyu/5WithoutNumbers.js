ee3de858ba0efcd3b87952cfd0c883f9a0489b89
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-27 10:06:16 +0200

    Add 5 without numbers !!

---
+++ 8-kyu/5WithoutNumbers.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+5 without numbers !!
+
+- DESCRIPTION:
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+- LABELS:
+#RESTRICTED #FUNDAMENTALS #PUZZLES
+
+*/
+
+function unusualFive() {
+  return "ABCDE".length;
+}
