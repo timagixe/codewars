6651af221d2da47f263e4586ceb5960dcb346864
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-28 10:15:53 +0200

    Add Beginner - Lost Without a Map

---
+++ 8-kyu/beginnerLostWithoutAMap.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+Beginner - Lost Without a Map
+
+- DESCRIPTION:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+- LABELS:
+#FUNDAMENTALS #ARRAYS
+
+*/
+
+function maps(nums) {
+  return nums.map((num) => num * 2);
+}
