e348c19afc8516e3ec2fef616b985df0ef363745
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-28 10:21:07 +0200

    Add Find the Remainder

---
+++ 8-kyu/findTheRemainder.js
@@ -0,0 +1,21 @@
+/*
+
+- TITLE:
+Find the Remainder
+
+- DESCRIPTION:
+Task:
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+function remainder(x, y) {
+  const max = Math.max(x, y);
+  const min = Math.min(x, y);
+  return max % min;
+}
