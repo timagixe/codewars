65ceeb7e574886144b3c903d43d0f8b789cdf5e8
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-28 10:24:35 +0200

    Add Thinkful - Logic Drills: Traffic light

---
+++ 8-kyu/thinkfulLogicDrillsTrafficLight.js
@@ -0,0 +1,29 @@
+/*
+
+- TITLE:
+Thinkful - Logic Drills: Traffic light
+
+- DESCRIPTION:
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+const [GREEN, YELLOW, RED] = ["green", "yellow", "red"];
+
+function updateLight(current) {
+  switch (current) {
+    case GREEN:
+      return YELLOW;
+    case YELLOW:
+      return RED;
+    default:
+      return GREEN;
+  }
+}
