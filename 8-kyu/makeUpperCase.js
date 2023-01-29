df52c7ff1ddf0438429a95e447e9b0d06ca18de0
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-29 17:34:55 +0200

    Add MakeUpperCase

---
+++ 8-kyu/makeUpperCase.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+MakeUpperCase
+
+- DESCRIPTION:
+Write a function which converts the input string to uppercase.
+
+- LABELS:
+#FUNDAMENTALS #STRINGS
+
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
