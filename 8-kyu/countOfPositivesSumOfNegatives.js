6ba25fd3f66d2fa507f9c74d285632552b827e97
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-30 12:18:06 +0200

    Add Count of positives / sum of negatives

---
+++ 8-kyu/countOfPositivesSumOfNegatives.js
@@ -0,0 +1,31 @@
+/*
+
+- TITLE:
+Count of positives / sum of negatives
+
+- DESCRIPTION:
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+- LABELS:
+#FUNDAMENTALS #ARRAYS #LISTS
+
+*/
+
+function countPositivesSumNegatives(values) {
+  if (!values || !values.length) return [];
+  return values.reduce(
+    (result, value) => {
+      if (value > 0) result[0] += 1;
+      if (value < 0) result[1] += value;
+      return result;
+    },
+    [0, 0]
+  );
+}
