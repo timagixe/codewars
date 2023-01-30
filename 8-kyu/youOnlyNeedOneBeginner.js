221f11a374c25f309c3b9ffc242d849c561891eb
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-30 12:12:18 +0200

    Add You only need one - Beginner

---
+++ 8-kyu/youOnlyNeedOneBeginner.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+You only need one - Beginner
+
+- DESCRIPTION:
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+- LABELS:
+#FUNDAMENTALS #STRINGS #ARRAYS
+
+*/
+
+function check(array, value) {
+  return array.indexOf(value) !== -1;
+}
