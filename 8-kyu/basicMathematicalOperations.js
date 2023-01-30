4e4b413498d455210c0327242ac1f79f536459be
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-30 12:25:13 +0200

    Add Basic Mathematical Operations

---
+++ 8-kyu/basicMathematicalOperations.js
@@ -0,0 +1,33 @@
+/*
+
+- TITLE:
+Basic Mathematical Operations
+
+- DESCRIPTION:
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+const OPERATIONS = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b
+};
+
+function basicOp(operation, value1, value2) {
+  const calc = OPERATIONS[operation];
+  return calc(value1, value2);
+}
