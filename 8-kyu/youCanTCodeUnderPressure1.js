f03c912e9a5bb6db196899bb7720823b1407e2ba
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-30 12:20:25 +0200

    Add You Can't Code Under Pressure #1

---
+++ 8-kyu/youCanTCodeUnderPressure1.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+You Can't Code Under Pressure #1
+
+- DESCRIPTION:
+Code as fast as you can! You need to double the integer and return it.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function doubleInteger(i) {
+  return i * 2;
+}
