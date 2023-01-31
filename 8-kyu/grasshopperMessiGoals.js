aef9d48902b3a4fdbcfae24ce08ba5544b16ec8d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-31 07:27:09 +0200

    Add Grasshopper - Messi Goals

---
+++ 8-kyu/grasshopperMessiGoals.js
@@ -0,0 +1,23 @@
+/*
+
+- TITLE:
+Grasshopper - Messi Goals
+
+- DESCRIPTION:
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
