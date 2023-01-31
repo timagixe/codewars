a68ac17fd06be5bcf32f35dc0809a68b00b2018f
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-31 07:28:18 +0200

    Add Reversed Strings

---
+++ 8-kyu/reversedStrings.js
@@ -0,0 +1,19 @@
+/*
+
+- TITLE:
+Reversed Strings
+
+- DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+- LABELS:
+#STRINGS #FUNDAMENTALS
+
+*/
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
