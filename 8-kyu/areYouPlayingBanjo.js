74bac9d5134c37eb7307f9cd7b2bf838924b869e
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-01-31 07:23:45 +0200

    Add Are You Playing Banjo?

---
+++ 8-kyu/areYouPlayingBanjo.js
@@ -0,0 +1,25 @@
+/*
+
+- TITLE:
+Are You Playing Banjo?
+
+- DESCRIPTION:
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+- LABELS:
+#STRINGS #FUNDAMENTALS
+
+*/
+
+function areYouPlayingBanjo(name) {
+  const char = name[0].toLowerCase();
+  if (char === "r") return `${name} plays banjo`;
+  return `${name} does not play banjo`;
+}
