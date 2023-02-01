dff8aeeab0437b48fd167c900fea580f4d86ea3e
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-01 19:49:50 +0200

    Add Basic variable assignment

---
+++ 8-kyu/basicVariableAssignment.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Basic variable assignment
+
+- DESCRIPTION:
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+- LABELS:
+#DEBUGGING
+
+*/
+
+const a = "code";
+const b = "wa.rs";
+const name = a + b;
