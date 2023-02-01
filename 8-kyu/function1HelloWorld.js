7b4671c0ad07fad171dbf8eae77487af53b70295
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-01 19:44:37 +0200

    Add Function 1 - hello world

---
+++ 8-kyu/function1HelloWorld.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+Function 1 - hello world
+
+- DESCRIPTION:
+Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+- LABELS:
+FUNDAMENTALS
+
+*/
+
+function greet() {
+  return "hello world!";
+}
