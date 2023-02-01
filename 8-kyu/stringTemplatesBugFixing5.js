bdcbb54f46773e0ae53672787a4526a910e46067
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-01 19:57:52 +0200

    Add String Templates - Bug Fixing #5

---
+++ 8-kyu/stringTemplatesBugFixing5.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+String Templates - Bug Fixing #5
+
+- DESCRIPTION:
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+- LABELS:
+#STRINGS #DEBUGGING
+
+*/
+
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
