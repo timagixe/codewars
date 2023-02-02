2ea637b1e7953f5dc62e52082d77eaaff0e7d6f1
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-02 09:49:56 +0200

    Add Is it a palindrome?

---
+++ 8-kyu/isItAPalindrome.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Is it a palindrome?
+
+- DESCRIPTION:
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function isPalindrome(str) {
+  const lowerCase = str.toLowerCase();
+  return lowerCase.split("").reverse().join("") === lowerCase;
+}
