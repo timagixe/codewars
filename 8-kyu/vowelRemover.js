1066cab28b2e4be2672f2d618104022f0da0ac52
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-03 23:04:25 +0200

    Add Vowel remover

---
+++ 8-kyu/vowelRemover.js
@@ -0,0 +1,27 @@
+/*
+
+- TITLE:
+Vowel remover
+
+- DESCRIPTION:
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata
+
+- LABELS:
+#STRINGS #FUNDAMENTALS
+
+*/
+
+const VOWEL_REGEXP = /[aeiou]/g;
+const EMPTY = "";
+
+function shortcut(string) {
+  return string.replace(VOWEL_REGEXP, EMPTY);
+}
