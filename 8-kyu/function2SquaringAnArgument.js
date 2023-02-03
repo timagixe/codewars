ce902d32e26a6a99cf6225a86160ce859a3a34f8
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-03 22:58:20 +0200

    Add Function 2 - squaring an argument

---
+++ 8-kyu/function2SquaringAnArgument.js
@@ -0,0 +1,16 @@
+/*
+
+- TITLE:
+Function 2 - squaring an argument
+
+- DESCRIPTION:
+Now you have to write a function that takes an argument and returns the square of it.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function square(x) {
+  return x ** 2;
+}
