7bcf52073a1f8ecfe805f05e4d2d1203c5859aae
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-04 15:03:06 +0200

    Add Beginner - Reduce but Grow

---
+++ 8-kyu/beginnerReduceButGrow.js
@@ -0,0 +1,18 @@
+/*
+
+- TITLE:
+Beginner - Reduce but Grow
+
+- DESCRIPTION:
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+- LABELS:
+#FUNDAMENTALS #ARRAYS
+
+*/
+
+function grow(nums) {
+  return nums.reduce((a, b) => a * b);
+}
