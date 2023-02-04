f4a540c9faf7001ebe84243eae3c13260596da57
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-04 16:01:04 +0200

    Add Third Angle of a Triangle

---
+++ 8-kyu/thirdAngleOfATriangle.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+Third Angle of a Triangle
+
+- DESCRIPTION:
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
