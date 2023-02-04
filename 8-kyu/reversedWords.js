ecee761d89af8a10e3da43e3fa041d00b3f8b849
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-04 16:05:32 +0200

    Add Reversed Words

---
+++ 8-kyu/reversedWords.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Reversed Words
+
+- DESCRIPTION:
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+- LABELS:
+#STRINGS #ALGORITHMS
+
+*/
+
+const SPACE = " ";
+
+function reverseWords(str) {
+  return str.split(SPACE).reverse().join(SPACE);
+}
