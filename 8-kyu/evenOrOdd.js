a3a437a36184f5b142d768e110b38fde9faa910a
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-04 15:39:41 +0200

    Add Even or Odd

---
+++ 8-kyu/evenOrOdd.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Even or Odd
+
+- DESCRIPTION:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+function evenOrOdd(number) {
+  if (number % 2) return "Odd";
+  return "Even";
+}
