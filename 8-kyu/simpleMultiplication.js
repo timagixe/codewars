b66be5a1a1e4431bfccfe5c65555c922c369c475
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-04 16:04:52 +0200

    Add Simple multiplication

---
+++ 8-kyu/simpleMultiplication.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Simple multiplication
+
+- DESCRIPTION:
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function simpleMultiplication(number) {
+  if (number % 2) return number * 9;
+  return number * 8;
+}
