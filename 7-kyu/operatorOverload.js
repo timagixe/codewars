3e6852e2ccd43eca49da41d5b64574ef9a550834
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-06 22:10:50 +0200

    Add Operator overload ?

---
+++ 7-kyu/operatorOverload.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+Operator overload ?
+
+- DESCRIPTION:
+We have a class named 'Foo' with a 'val' attribute.
+
+var foo = new Foo(2);     // foo.val = 2;
+var boo = new Foo(3);    // boo.val = 3;
+We want to be able to sum two Foo object using the '+' operator.
+
+foo + boo // => must return 5
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+class Foo {
+  constructor(value) {
+    this.val = value;
+  }
+
+  toString() {
+    return Number(this.val);
+  }
+}
