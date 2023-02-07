e18394587bbf7a7c966c38904a002a84d07052a0
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-07 22:32:35 +0200

    Add esreveR gnirtS

---
+++ 7-kyu/esreveRGnirtS.js
@@ -0,0 +1,20 @@
+/*
+
+- TITLE:
+esreveR gnirtS
+
+- DESCRIPTION:
+Create a function called reverse for the String prototype that will allow the following functionality:
+
+"String".reverse();// => returns "gnirtS"
+"Super awesome string".reverse();// => returns "gnirts emosewa repuS"
+The reverse function should NOT modify the original string.
+
+- LABELS:
+#STRINGS #FUNDAMENTALS
+
+*/
+
+String.prototype.reverse = function () {
+  return this.split("").reverse().join("");
+};
