d19d995a5cd1dc3d9ea02bcc625944052b589c5d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-07 22:35:07 +0200

    Add Honey to the Bee

---
+++ 7-kyu/honeyToTheBee.js
@@ -0,0 +1,33 @@
+/*
+
+- TITLE:
+Honey to the Bee
+
+- DESCRIPTION:
+Find out why the amount of honey in the hive is not increasing.
+
+- LABELS:
+#DEBUGGING
+
+*/
+
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+Hive.prototype.addPollen = function (pollen) {
+  this.pollen += pollen;
+};
+
+Hive.prototype.getPollen = function () {
+  return this.pollen;
+};
+
+Bee.prototype.unloadPollen = function () {
+  this.hive.addPollen(this.capacity);
+};
