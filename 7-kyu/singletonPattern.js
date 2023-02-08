c1ed1ec98a9f879d3ef212cb14fb862fb75ca859
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-08 11:46:15 +0200

    Add Singleton Pattern

---
+++ 7-kyu/singletonPattern.js
@@ -0,0 +1,32 @@
+/*
+
+- TITLE:
+Singleton Pattern
+
+- DESCRIPTION:
+In software engineering, the singleton pattern is a design pattern that restricts the instantiation of a class to one object. This is useful when exactly one object is needed to coordinate actions across the system.
+
+Create an Singleton pattern, so there is one object in system.
+
+Example:
+
+var obj1 = new Singleton();
+var obj2 = new Singleton();
+obj1 === obj2; // => true
+obj1.test = 1;
+obj2.test; // => 1
+
+- LABELS:
+#SINGLETON #OBJECT_ORIENTED_PROGRAMMING #FUNDAMENTALS
+
+*/
+
+let instance;
+class Singleton {
+  constructor() {
+    if (!instance) {
+      instance = this;
+    }
+    return instance;
+  }
+}
