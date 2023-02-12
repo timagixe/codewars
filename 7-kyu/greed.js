7dd5e8d815cd701be444cbe71508fbb047fd94c7
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-12 19:13:52 +0200

    Add Greed

---
+++ 7-kyu/greed.js
@@ -0,0 +1,24 @@
+/*
+
+- TITLE:
+Greed
+
+- DESCRIPTION:
+In my "getQuotes" function, I am trying to extract only the quotes from a string.
+
+#Example
+
+getQuotes('"example quote #1" some text "example quote #2"'); 
+// should return ["\"example quote #1\"","\"example quote #2\""]
+However, my test results are not as expected. can you find out what I did wrong?
+
+- LABELS:
+#REGULAR_EXPRESSIONS #DEBUGGING
+
+*/
+
+const TWO_CHARS_REG_EXP = /"[^"]*"/g;
+
+function getQuotes(string) {
+  return string.match(TWO_CHARS_REG_EXP);
+}
