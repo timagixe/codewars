35c188a255f3a13ea3a7db562146f7d693e7e6e3
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-12 19:52:56 +0200

    Add Vampire Numbers

---
+++ 7-kyu/vampireNumbers.js
@@ -0,0 +1,32 @@
+/*
+
+- TITLE:
+Vampire Numbers
+
+- DESCRIPTION:
+Vampire Numbers
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function vampire_test(a, b) {
+  const multiplicants = join(`${a}${b}`);
+  const product = join(`${a * b}`);
+  return multiplicants === product;
+}
+
+function join(string) {
+  return string.split("").sort().join("");
+}
