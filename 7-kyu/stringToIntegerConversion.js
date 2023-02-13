9fe87f865050a3ea248940bd3047331df220f3d3
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-13 11:43:00 +0200

    Add String to integer conversion

---
+++ 7-kyu/stringToIntegerConversion.js
@@ -0,0 +1,34 @@
+/*
+
+- TITLE:
+String to integer conversion
+
+- DESCRIPTION:
+JavaScript provides a built-in parseInt method.
+
+It can be used like this:
+
+parseInt("10") returns 10
+parseInt("10 apples") also returns 10
+We would like it to return "NaN" (as a string) for the second case because the input string is not a valid number.
+
+You are asked to write a myParseInt method with the following rules:
+
+It should make the conversion if the given string only contains a single integer value (and possibly spaces - including tabs, line feeds... - at both ends)
+For all other strings (including the ones representing float values), it should return NaN
+It should assume that all numbers are not signed and written in base 10
+
+- LABELS:
+#STRINGS #REGULAR_EXPRESSIONS #FUNDAMENTALS
+
+*/
+
+function myParseInt(str) {
+  const trimmed = str.trim();
+  if (hasChars(trimmed)) return NaN;
+  return parseInt(trimmed);
+}
+
+function hasChars(str) {
+  return /[^0-9]/g.test(str);
+}
