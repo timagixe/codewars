05a47c522f5cb41d3e8eab9bac768d448f3d2dc1
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-14 11:52:07 +0200

    Add Reverse fibonacci series

---
+++ 7-kyu/reverseFibonacciSeries.js
@@ -0,0 +1,26 @@
+/*
+
+- TITLE:
+Reverse fibonacci series
+
+- DESCRIPTION:
+Write a function to generate 'n' number of fibonacci series (0,1,1,2,3...) in reverse order . The output should be a string of fibonacci series in the reverse order upto the given number.
+
+Example
+reverseFibo(3)  // => '110'
+reverseFibo(10) // => '3421138532110'
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function reverseFibo(n) {
+  if (n <= 1) return "0";
+  const nums = [1, 0];
+  while (n > nums.length) {
+    const nextNum = nums[0] + nums[1];
+    nums.unshift(nextNum);
+  }
+  return nums.join("");
+}
