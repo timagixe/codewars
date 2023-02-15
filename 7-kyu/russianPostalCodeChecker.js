fa550a02d558b08473a0f7bcec4d98f4d5876f9d
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-15 09:43:55 +0200

    Add Russian postal code checker

---
+++ 7-kyu/russianPostalCodeChecker.js
@@ -0,0 +1,35 @@
+/*
+
+- TITLE:
+Russian postal code checker
+
+- DESCRIPTION:
+You should write a simple function that takes string as input and checks if it is a valid Russian postal code, returning true or false.
+
+A valid postcode should be 6 digits with no white spaces, letters or other symbols. Empty string should also return false.
+
+Please also keep in mind that a valid post code cannot start with 0, 5, 7, 8 or 9
+
+Examples
+Valid postcodes:
+
+198328
+310003
+424000
+Invalid postcodes:
+
+056879
+12A483
+1@63
+111
+
+- LABELS:
+#REGULAR_EXPRESSIONS #STRINGS #FUNDAMENTALS
+
+*/
+
+const POST_CODE_REGEXP = /^[12346]\d{5}$/;
+
+function zipvalidate(postcode) {
+  return POST_CODE_REGEXP.test(postcode);
+}
