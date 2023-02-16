e7ac0c71154c204c1bf499550b616f4768f579a8
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-16 10:22:54 +0200

    Add Convert between radians and degrees

---
+++ 7-kyu/convertBetweenRadiansAndDegrees.js
@@ -0,0 +1,29 @@
+/*
+
+- TITLE:
+Convert between radians and degrees
+
+- DESCRIPTION:
+Extend the Math object (JS) or module (Ruby and Python) to convert degrees to radians and viceversa. The result should be rounded to two decimal points. Note that all methods of Math object are static.
+
+Example:
+
+Math.degrees(Math.PI) //180deg
+Math.radians(180) //3.14rad
+
+- LABELS:
+#MATHEMATICS #FUNDAMENTALS
+
+*/
+
+Math.degrees = function (radians) {
+  const value = (radians * 180) / Math.PI;
+  const fixed = parseFloat(value.toFixed(2));
+  return `${fixed}deg`;
+};
+
+Math.radians = function (degrees) {
+  const value = (degrees * Math.PI) / 180;
+  const fixed = parseFloat(value.toFixed(2));
+  return `${fixed}rad`;
+};
