1aff8c4f93a16a334b12bc5acb2387051908d74b
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-17 09:18:58 +0200

    Add Sum ALL the arrays!

---
+++ 7-kyu/sumAllTheArrays.js
@@ -0,0 +1,25 @@
+/*
+
+- TITLE:
+Sum ALL the arrays!
+
+- DESCRIPTION:
+You are given an array of values.
+
+Sum every number value in the array, and any nested arrays (to any depth).
+
+Ignore all other types of values.
+
+- LABELS:
+#RECURSION #ARRAYS #FUNDAMENTALS
+
+*/
+
+function arraySum(arr) {
+  return arr.flat(Infinity).reduce(add, 0);
+}
+
+function add(sum, num) {
+  if (!Number.isFinite(num)) return sum;
+  return sum + num;
+}
