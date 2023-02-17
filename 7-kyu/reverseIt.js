c41b75491ea90746e4941b32d8151bb893b31573
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-17 08:54:29 +0200

    Add reverseIt

---
+++ 7-kyu/reverseIt.js
@@ -0,0 +1,41 @@
+/*
+
+- TITLE:
+reverseIt
+
+- DESCRIPTION:
+You have to create a function named reverseIt.
+
+Write your function so that in the case a string or a number is passed in as the data , you will return the data in reverse order. If the data is any other type, return it as it is.
+
+Examples of inputs and subsequent outputs:
+
+"Hello" -> "olleH"
+
+"314159" -> "951413"
+
+[1,2,3] -> [1,2,3]
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function reverseIt(data) {
+  switch (typeof data) {
+    case "string":
+      return reverseString(data);
+    case "number":
+      return reverseNumber(data);
+    default:
+      return data;
+  }
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function reverseNumber(num) {
+  return Number(reverseString(num.toString()));
+}
