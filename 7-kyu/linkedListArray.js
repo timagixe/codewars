8ddee44f62e281b08cc5bb72dcd5af22ab1b7afa
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-17 09:12:42 +0200

    Add LinkedList -> Array

---
+++ 7-kyu/linkedListArray.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+LinkedList -> Array
+
+- DESCRIPTION:
+Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+
+Here's an example of a list:
+
+{value: 1, next: {value: 2, next: {value: 3, next: null}}}
+Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+
+[1, 2, 3]
+Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+
+- LABELS:
+#DATA_STRUCTURES #ARRAYS #FUNDAMENTALS
+
+*/
+
+function listToArray(list) {
+  const arr = [];
+  for (let node = list; node !== null; node = node.next) {
+    arr.push(node.value);
+  }
+  return arr;
+}
