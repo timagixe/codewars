b96a19e8820add96117d0428d15339285476fdcc
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-18 22:59:16 +0200

    Add Is this working?

---
+++ 7-kyu/isThisWorking.js
@@ -0,0 +1,21 @@
+/*
+
+- TITLE:
+Is this working?
+
+- DESCRIPTION:
+Rewriting selfies
+Let's get rid of the `self` usage, by using arrow functions!
+This can be achieved without arrow functions as well, but the point would be to learn the differences.
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function Counter() {
+  this.count = 0;
+  this.updateCount = () => this.count++;
+}
