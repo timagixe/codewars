945cca50531c19dc463547c5080bcc92305122df
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-18 10:06:32 +0200

    Add Fix my method

---
+++ 7-kyu/fixMyMethod.js
@@ -0,0 +1,27 @@
+/*
+
+- TITLE:
+Fix my method
+
+- DESCRIPTION:
+I am kind of new to coding so I'm not sure what is wrong with my code.
+
+Property objMethod should be called by myFunction.
+
+Can you fix the syntax so myFunction will be working again? Please check things like braces, commas, and letter case.
+
+IMPORTANT: Returning just "string" is insufficient. We are required to use Object Literal Notation.
+
+- LABELS:
+#DEBUGGING #OBJECT_ORIENTED_PROGRAMMING
+
+*/
+
+function myFunction() {
+  return {
+    objProperty: "string",
+    objMethod: function () {
+      return this.objProperty;
+    }
+  };
+}
