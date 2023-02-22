1d463cd0d72ab93f797f77e2530c7b38bc1a354e
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-22 10:04:40 +0200

    Add Regex Failure - Bug Fixing #2

---
+++ 7-kyu/regexFailureBugFixing2.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Regex Failure - Bug Fixing #2
+
+- DESCRIPTION:
+Regex Failure - Bug Fixing #2
+Oh no, Timmy's received some hate mail recently but he knows better. Help Timmy fix his regex filter so he can be awesome again!
+
+- LABELS:
+#DEBUGGING
+
+*/
+
+function filterWords(phrase) {
+  return phrase.replace(/(bad|mean|ugly|horrible|hideous)/gi, "awesome");
+}
