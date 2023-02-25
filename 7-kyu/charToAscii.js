8b15e0c3c544476e22d7000838e6fce822e8d2ab
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-25 21:36:06 +0200

    Add char_to_ascii

---
+++ 7-kyu/charToAscii.js
@@ -0,0 +1,22 @@
+/*
+
+- TITLE:
+char_to_ascii
+
+- DESCRIPTION:
+Take a string and return a hash with all the ascii values of the characters in the string. Returns nil if the string is empty. The key is the character, and the value is the ascii value of the character. Repeated characters are to be ignored and non-alphebetic characters as well.
+
+- LABELS:
+#STRINGS #PARSING #FUNDAMENTALS
+
+*/
+
+const CHARS_REGEXP = /[a-z]/gi;
+
+function charToAscii(string) {
+  if (!string.length) return null;
+  return string.match(CHARS_REGEXP).reduce((hashMap, char) => {
+    hashMap[char] = char.charCodeAt();
+    return hashMap;
+  }, {});
+}
