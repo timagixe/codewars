eb6bde19ffcbb909ebfe2836e7f6b43c8934b01f
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-27 11:22:43 +0200

    Add Trigrams

---
+++ 7-kyu/trigrams.js
@@ -0,0 +1,34 @@
+/*
+
+- TITLE:
+Trigrams
+
+- DESCRIPTION:
+Trigrams are a special case of the n-gram, where n is 3. One trigram is a continous sequence of 3 chars in phrase. Wikipedia
+
+return all trigrams for the given phrase
+replace spaces with underscore (_)
+return an empty string for phrases shorter than 3
+Example:
+
+"the quick red" --> "the he_ e_q _qu qui uic ick ck_ k_r _re red"
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function trigrams(phrase) {
+  if (phrase.length <= 3) return "";
+  const chunks = [];
+  const text = phrase.replace(/ /g, "_");
+  for (let index = 0; index < text.length - 2; index++) {
+    const chunk = getTrigram(text, index, index + 3);
+    chunks.push(chunk);
+  }
+  return chunks.join(" ");
+}
+
+function getTrigram(string, start, end) {
+  return string.slice(start, end);
+}
