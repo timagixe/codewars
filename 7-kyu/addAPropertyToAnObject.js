05ee3ae6b24e239dded530e65cbf1e033cd23958
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-02-28 16:55:42 +0200

    Add Add a property to an object

---
+++ 7-kyu/addAPropertyToAnObject.js
@@ -0,0 +1,17 @@
+/*
+
+- TITLE:
+Add a property to an object
+
+- DESCRIPTION:
+Write a function that adds a named property to an object. It must be possible to set the property to a new value. If the property already exists on the object, and error should be thrown.
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function addProperty(obj, prop, value) {
+  if (prop in obj) throw new Error(`Property ${prop} already exists.`);
+  obj[prop] = value;
+}
