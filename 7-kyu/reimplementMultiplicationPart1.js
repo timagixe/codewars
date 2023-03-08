0a7f26a7fe820219de2c59d6230b3f1e58cd02cf
Tima Gixe <timagixe@protonmail.com>
2023-03-08 18:28:12 +0000

    Add Reimplement Multiplication Part 1

---
+++ 7-kyu/reimplementMultiplicationPart1.js
@@ -0,0 +1,21 @@
+/*
+
+- TITLE:
+Reimplement Multiplication Part 1
+
+- DESCRIPTION:
+Define a function mul(a, b) that takes two non-negative integers a and b and returns their product.
+
+You should only use the + and/or - operators. Using * is cheating!
+
+You can do this iteratively or recursively.
+
+- LABELS:
+#MATHEMATICS #ALGORITHMS
+
+*/
+
+function mul(a, b, acc = 0) {
+  if (a === 0) return acc;
+  return mul(a - 1, b, acc + b);
+}
