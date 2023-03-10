64f0f27f4c1b62e43a59f783cb990cec55f1c6d1
Tima Gixe <timagixe@protonmail.com>
2023-03-10 12:09:15 +0000

    Add Array arithmetic

---
+++ 7-kyu/arrayArithmetic.js
@@ -0,0 +1,22 @@
+/*
+
+- TITLE:
+Array arithmetic
+
+- DESCRIPTION:
+Enable arithmetic operations on arrays, where the value of an array is given by the sum of its elements. Examples:
+
+[1,2,3] + 4 = 10
+[1,1,1] - 3 = 0
+['a','b'] + 'c' = 'abc'
+PS: Due to intrinsic uncertainty of empty arrays, in this kata an empty array is treated as 0. For hints see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf
+
+- LABELS:
+#ARRAYS #FUNDAMENTALS
+
+*/
+
+Array.prototype.valueOf = function () {
+  if (!this.length) return 0;
+  return this.reduce((a, b) => a + b);
+};
