c0e845be7e5989a4680ea5f7742d88071e8c1cab
Tima Gixe <timagixe@protonmail.com>
2023-03-11 19:09:53 +0000

    Add Is Undefined?

---
+++ 7-kyu/isUndefined.js
@@ -0,0 +1,19 @@
+/*
+
+- TITLE:
+Is Undefined?
+
+- DESCRIPTION:
+Is Undefined?
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+
+- LABELS:
+#FUNDAMENTALS
+
+*/
+
+function isUndefined(value) {
+  return value === undefined;
+}
