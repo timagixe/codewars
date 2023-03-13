b5c2633f41e9698531e7ed97b05163d3ec12a507
Tima Gixe <timagixe@protonmail.com>
2023-03-13 09:10:44 +0000

    Add Create sequence containing all allowed characters

---
+++ 7-kyu/createSequenceContainingAllAllowedCharacters.js
@@ -0,0 +1,28 @@
+/*
+
+- TITLE:
+Create sequence containing all allowed characters
+
+- DESCRIPTION:
+Define createSequence(regex) function that returns a string of all characters (in ASCII order) matching specified regular expression one-character criterion.
+
+Examples
+let digits = /[0-9]/;
+// createSequence(digits) === '0123456789'
+
+let hexadecimal = /[0-9A-F]/;
+// createSequence(hexadecimal) === '0123456789ABCDEF'
+
+- LABELS:
+#REGULAR_EXPRESSIONS #FUNDAMENTALS
+
+*/
+
+function createSequence(regex) {
+  let result = "";
+  for (let code = 0; code < 128; code++) {
+    const char = String.fromCharCode(code);
+    if (regex.test(char)) result += char;
+  }
+  return result;
+}
