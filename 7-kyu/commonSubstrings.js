11a2bc73a995afb484e9ff4efe8bfd99acaa47b6
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-07-19 21:14:12 +0300

    Add Common Substrings

---
+++ 7-kyu/commonSubstrings.js
@@ -0,0 +1,49 @@
+/*
+
+- TITLE:
+Common Substrings
+
+- DESCRIPTION:
+Given 2 strings, your job is to find out if there is a substring that appears in both strings. You will return true if you find a substring that appears in both strings, or false if you do not. We only care about substrings that are longer than one letter long.
+
+#Examples:
+
+*Example 1*
+SubstringTest("Something","Fun"); //Returns false
+
+*Example 2*
+SubstringTest("Something","Home"); //Returns true
+In the above example, example 2 returns true because both of the inputs contain the substring "me". (soMEthing and hoME)
+In example 1, the method will return false because something and fun contain no common substrings. (We do not count the 'n' as a substring in this Kata because it is only 1 character long)
+
+#Rules: Lowercase and uppercase letters are the same. So 'A' == 'a'.
+We only count substrings that are > 1 in length.
+
+#Input: Two strings with both lower and upper cases. #Output: A boolean value determining if there is a common substring between the two inputs.
+
+- LABELS:
+#STRINGS #ALGORITHMS
+
+*/
+
+function reduceString(string, callback) {
+    let callbackResult = false;
+
+    for (let i = 0, j = i + 2; i < string.length && j <= string.length; i++, j++) {
+        const slice = string.slice(i, j);
+        callbackResult = callback(slice);
+        if (callbackResult) return callbackResult;
+    }
+
+    return callbackResult;
+}
+
+function SubstringTest(str1, str2) {
+    const substrings = new Set();
+
+    reduceString(str2.toLowerCase(), (slice) => {
+        if (slice.length === 2) substrings.add(slice);
+    });
+
+    return reduceString(str1.toLowerCase(), (slice) => substrings.has(slice));
+}
