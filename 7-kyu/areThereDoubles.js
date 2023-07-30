3ee600fffb03b69e716acd17e4da089d0e9be812
Tima Gixe <60817786+timagixe@users.noreply.github.com>
2023-07-30 17:33:48 +0300

    Add Are there doubles?

---
+++ 7-kyu/areThereDoubles.js
@@ -0,0 +1,46 @@
+/*
+
+- TITLE:
+Are there doubles?
+
+- DESCRIPTION:
+Your job is to build a function which determines whether or not there are double characters in a string (including whitespace characters). For example aa, !! or   .
+
+You want the function to return true if the string contains double characters and false if not. The test should not be case sensitive; for example both aa & aA return true.
+
+Examples:
+
+  doubleCheck("abca")
+  //returns false
+
+  doubleCheck("aabc")
+  //returns true
+
+  doubleCheck("a 11 c d")
+  //returns true
+
+  doubleCheck("AabBcC")
+  //returns true
+
+  doubleCheck("a b  c")
+  //returns true
+
+  doubleCheck("a b c d e f g h i h k")
+  //returns false
+
+  doubleCheck("2020")
+  //returns false
+
+  doubleCheck("a!@€£#$%^&*()_-+=}]{[|\"':;?/>.<,~")
+  //returns false
+
+- LABELS:
+#REGULAR_EXPRESSIONS #FUNDAMENTALS
+
+*/
+
+const DOUBLES_REGEXP = /(.)\1/i;
+
+function doubleCheck(str) {
+    return DOUBLES_REGEXP.test(str);
+}
